7765d9fed25a922345642da6466d042825ba974a
Linda Bergen <linda.bergen@mobilejob.com>
2018-08-19 17:11:40 +0200

    initialize package object in controller, add parsing logic to package model

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,5 +1,10 @@
+require 'rubygems'
+require 'HTTParty'
+require 'dcf'
+
 class PackagesController < ApplicationController
   def index
+    @package = Package.new.parse
     @packages = Package.all
   end
 end
--- app/models/package.rb
+++ app/models/package.rb
@@ -1,2 +1,22 @@
+require 'rubygems'
+require 'HTTParty'
+require 'dcf'
+
 class Package < ApplicationRecord
+  include HTTParty
+  base_uri 'http://cran.r-project.org'
+
+  def parse
+    response = HTTParty.get('http://cran.r-project.org/src/contrib/PACKAGES')
+    parsed_response = Dcf.parse response.body
+    add_packages(parsed_response)
+  end
+
+  def add_packages(parsed_response)
+    parsed_response[0..49].each do |res|
+      @package = Package.new
+      @package.update_attributes(name: res["Package"], version: res["Version"])
+      @package.save!
+    end
+  end
 end
